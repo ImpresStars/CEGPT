e6a281b29dcca039bc5eec555a2773e432c7b67e
esteban sustacha <esustachah@gmail.com>
2025-01-09 16:31:50 -0500

    Initial project upload
    everything less .env

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app min-h-screen flex flex-col">
+    <template v-if="!isInDashboard">
+      <Header 
+        :is-logged-in="isAuthenticated" 
+        :user-type="userType"
+        @logout="handleLogout" 
+      />
+      <main class="flex-grow">
+        <router-view></router-view>
+      </main>
+      <Footer />
+    </template>
+    <template v-else>
+      <router-view></router-view>
+    </template>
+    <LoadingOverlay />
+    <ErrorBoundary />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute, useRouter } from 'vue-router'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import LoadingOverlay from './components/LoadingOverlay.vue'
+import ErrorBoundary from './components/ErrorBoundary.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    LoadingOverlay,
+    ErrorBoundary
+  },
+  setup() {
+    const store = useStore()
+    const route = useRoute()
+    const router = useRouter()
+    
+    const isAuthenticated = computed(() => store.getters['auth/isAuthenticated'])
+    const userType = computed(() => 'client')
+    const isInDashboard = computed(() => route.path.startsWith('/dashboard'))
+
+    const handleLogout = async () => {
+      const result = await store.dispatch('auth/logout')
+      if (result.success) {
+        await router.push('/login')
+      }
+    }
+
+    return {
+      isAuthenticated,
+      userType,
+      isInDashboard,
+      handleLogout
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
